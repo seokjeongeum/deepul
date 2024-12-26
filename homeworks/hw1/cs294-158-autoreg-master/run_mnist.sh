ac21600edd96a07a94c25247a718ffe5ccf35c95
root <jeseok@dblab.postech.ac.kr>
2024-12-26 18:25:17 +0900

    Remove deprecated files and update .gitignore for hw1 and hw2

--- homeworks/hw1/cs294-158-autoreg-master/run_mnist.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-python train_mnist.py --model rnn
-python train_mnist.py --model rnn_loc
-python train_mnist.py --model made --order_id 0
-python train_mnist.py --model pixelcnn
-python train_mnist.py --model wavenet
-python train_mnist.py --model wavenet_loc
-python train_mnist.py --model transformer
